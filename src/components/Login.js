fe256624e579710372f86cac18f8dbe84029319b
Divya124-di <divyafiitjee12@gmail.com>
2024-08-25 14:43:30 +0530

    signin Form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+  const [isSignUp , setIsSignUp] = useState(0);
+    const toggleSignup = () => {
+     setIsSignUp(!isSignUp);
+    };
+  
   return (
     <div>
       <Header />
@@ -12,21 +17,36 @@ const Login = () => {
           alt="img"
         />
       </div>
-      <div className='flex flex-col min-h-screen justify-center items-center'>
-        <form className="absolute bg-black w-3/12 flex flex-col gap-2 h-3/6 lg:p-12 sm:p-6">
+      <div className="flex flex-col min-h-screen justify-center items-center">
+        <form className=" p-8 absolute bg-black w-3/12 flex flex-col gap-2  text-white rounded-lg bg-opacity-80 lg:p-12 sm:p-6 ">
+          <h1 className="font-bold text-3xl my-2">
+            {isSignUp ? "Sign in" : "Sign up"}
+          </h1>
+          {!isSignUp && (
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="p-2 my-2  bg-gray-700 rounded-lg"
+            />
+          )}
           <input
             type="text"
             placeholder="Email"
-            className="p-2 m-2  bg-gray-700"
+            className="p-2 my-2  bg-gray-700 rounded-lg"
           />
           <input
             type="password"
             placeholder="Password"
-            className="p-2 m-2  bg-gray-700"
+            className="p-2 my-2  bg-gray-700 rounded-lg"
           />
-          <button type="submit" className="bg-red-700 p-2 m-2">
-            Sign in
+          <button type="submit" className="bg-red-700 p-2 my-2 rounded-lg">
+            {isSignUp ? "Sign in" : "Sign up"}
           </button>
+          <p className="cursor-pointer" onClick={toggleSignup}>
+            {isSignUp
+              ? " New to Netflix? sign up"
+              : "Already Registered Sign In Now"}
+          </p>
         </form>
       </div>
     </div>
